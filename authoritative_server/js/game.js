3b23153207aa15293c4f620c427722330ecf133b
Phil <knamustarsunder@gmail.com>
2020-10-17 14:08:36 -0700

    added card loader and flipping code

--- authoritative_server/js/game.js
+++ authoritative_server/js/game.js
@@ -9,7 +9,9 @@ const config = {
     default: 'arcade',
     arcade: {
       debug: false,
-      gravity: { y: 0 }
+      gravity: {
+        y: 0
+      }
     }
   },
   scene: {
@@ -22,6 +24,7 @@ const config = {
 
 function preload() {
   this.load.image('ship', 'assets/spaceShips_001.png');
+  this.load.atlas('cards', 'assets/atlas/cards.png', 'assets/atlas/cards.json');
 }
 
 function create() {
@@ -29,6 +32,8 @@ function create() {
   this.players = this.physics.add.group();
 
   this.physics.add.collider(this.players);
+
+  loadCards(self);
 
   io.on('connection', function (socket) {
     console.log('a user connected');
@@ -62,6 +67,8 @@ function create() {
       handlePlayerInput(self, socket.id, inputData);
     });
   });
+
+
 }
 
 function update() {
@@ -97,6 +104,63 @@ function removePlayer(self, playerId) {
     if (playerId === player.playerId) {
       player.destroy();
     }
+  });
+}
+
+function loadCards(self) {
+  let frames = self.textures.get('cards').getFrameNames();
+
+  let x = 100;
+  let y = 100;
+  let cardNames = ['back', 
+    'clubsAce', 'clubs2', 'clubs3', 'clubs4', 'clubs5', 'clubs6', 'clubs7', 'clubs8', 'clubs9', 'clubs10', 'clubsJack', 'clubsQueen', 'clubsKing',
+    'diamondsAce', 'diamonds2', 'diamonds3', 'diamonds4', 'diamonds5', 'diamonds6', 'diamonds7','diamonds8', 'diamonds9', 'diamonds10', 'diamondsJack', 'diamondsQueen', 'diamondsKing',
+    'heartsAce', 'hearts2', 'hearts3', 'hearts4', 'hearts5', 'hearts6', 'hearts7', 'hearts8', 'hearts9', 'hearts10', 'heartsJack', 'heartsQueen', 'heartsKing',
+    'spadesAce', 'spades2', 'spades3', 'spades4', 'spades5', 'spades6', 'spades7', 'spades8', 'spades9', 'spades10', 'spadesJack', 'spadesQueen', 'spadesKing',
+    'joker'
+  ];
+
+  //add 52 playing cards in order
+  for (let i = 1; i <= 52; i++) {
+    let nextCard = frames[frames.indexOf(cardNames[i])];
+    let image = self.add.image(x, y, 'cards', nextCard).setInteractive();
+    image.cardName = cardNames[i];
+    self.input.setDraggable(image);
+
+    x += 35;
+    if (i % 13 == 0) {
+      x = 100;
+      y += 80;
+    }
+  }
+
+  //display joker card
+  x += 35;
+  y += 80;
+  nextCard = frames[frames.indexOf("joker")];
+  image = self.add.image(x, y, 'cards', nextCard).setInteractive();
+  image.cardName = "joker";
+
+  self.input.setDraggable(image);
+
+  self.input.mouse.disableContextMenu();
+
+  self.input.on('pointerdown', function (pointer, targets) {
+    if (pointer.rightButtonDown()) {
+      if (targets[0].cardName == targets[0].frame.name) { //if target cardName == frame name, flip it to back
+        targets[0].setFrame(frames[frames.indexOf("back")]);
+      } else { //otherwise flip card to front
+        targets[0].setFrame(frames[frames.indexOf(targets[0].cardName)]);
+      }
+    }
+  });
+
+  self.input.topOnly = true;
+
+  self.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+    gameObject.x = dragX;
+    gameObject.y = dragY;
+
   });
 }
 
