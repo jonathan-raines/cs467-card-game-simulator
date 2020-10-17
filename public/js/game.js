820e966685043fe425ea4f02ebdf2212600eec16
Jonathan Raines <jonathan.s.raines@gmail.com>
2020-10-16 21:13:02 -0500

    removed most of existing game functionality

--- public/js/game.js
+++ public/js/game.js
@@ -25,12 +25,12 @@ function create() {
 
   this.socket.on('currentPlayers', function (players) {
     Object.keys(players).forEach(function (id) {
-        displayPlayers(self, players[id], 'ship');
+      displayPlayers(self, players[id], 'ship');
     });
   });
 
   this.socket.on('newPlayer', function (playerInfo) {
-    displayPlayers(self, playerInfo, 'otherPlayer');
+    displayPlayers(self, playerInfo, 'ship');
   });
 
   this.socket.on('disconnect', function (playerId) {
@@ -48,10 +48,8 @@ function create() {
         //   ▼ auth players          ▼ local players
         if (players[id].playerId === player.playerId) {
           // Updates rotation and position
-          player.setRotation(players[id].rotation);
           player.setPosition(players[id].x, players[id].y);
         }
-
       });
     });
   });
@@ -60,8 +58,7 @@ function create() {
 function update() {}
 
 function displayPlayers(self, playerInfo, sprite) {
-  const player = self.add.sprite(playerInfo.x, playerInfo.y, sprite).setOrigin(0.5, 0.5).setDisplaySize(53, 40);
-
+  const player = self.add.sprite(playerInfo.x, playerInfo.y, sprite).setOrigin(0.5, 0.5);
   player.playerId = playerInfo.playerId;
   self.players.add(player);
 }
