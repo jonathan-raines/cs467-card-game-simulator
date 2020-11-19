75fd607900bec9b31735263758297f6baa4e2db0
Thomas Sugimoto <sugimotothomas@gmail.com>
2020-11-19 12:34:15 -0800

    Fixed stack visual effect with player rotation

--- public/js/cards.js
+++ public/js/cards.js
@@ -12,6 +12,8 @@ import {
     flipHandObject
 } from './hands.js';
 
+import { playerRotation } from './gameUI.js';
+
 // CONSTANTS
 export const MENU_DEPTH = 1000;
 export const CURSOR_DEPTH = 950;
@@ -158,7 +160,7 @@ export function addObject(self, spriteIds, x, y, spriteOrientations) {
       spritesToAdd[i] = createSprite(self, spriteId, cardNames[spriteId], spriteOrientations[i]);
 
       // Stack's Parallax Visual Effect 
-      stackVisualEffect(spritesToAdd[i], i, spriteIds.length-1);
+      stackVisualEffect(self, spritesToAdd[i], 0, i, spriteIds.length-1);
   }
   // Create a stack-like object (can have multiple sprites in it)/(No physics for client side)
   const object = self.add.container(x, y, spritesToAdd); // Server will move it with 'ObjectUpdates'
@@ -193,16 +195,20 @@ function updateStackVisualEffect(self, object) {
   var pos = 0;
   var size = object.length-1;
   object.getAll().forEach(function (sprite) {
-    stackVisualEffect(sprite, pos, size);
+    stackVisualEffect(self, sprite, object.angle, pos, size);
     pos++;
   });
 }
 
 // Makes a stack of cards look 3D
-export function stackVisualEffect(sprite, pos, size) {
-  if(sprite) {
-    sprite.x = -Math.floor((size-pos)/12);
-    sprite.y = Math.floor((size-pos)/5);
+export function stackVisualEffect(self, sprite, parentAngle, pos, size) {
+  if(sprite && players[self.socket.id]) {
+    var preX = -Math.floor((size-pos)/10);
+    var preY = Math.floor((size-pos)/4);
+    //console.log("parentAngle=" + object.angle);
+    var angle = Phaser.Math.DegToRad(parentAngle + playerRotation);
+    sprite.x = Math.cos(angle) * preX + Math.sin(angle) * preY;
+    sprite.y = Math.cos(angle) * preY - Math.sin(angle) * preX;
   }
 }
 
