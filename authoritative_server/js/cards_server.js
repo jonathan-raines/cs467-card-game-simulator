9e58a713abd0f79da2a9914b64897cecfe22787c
Thomas Sugimoto <sugimotothomas@gmail.com>
2020-11-19 19:35:32 -0800

    Remove console.log

--- authoritative_server/js/cards_server.js
+++ authoritative_server/js/cards_server.js
@@ -289,7 +289,6 @@ function setTableObjectPosition(self, objectId, xPos, yPos) {
 // Increments the overall depth by one and checks to see if it needs to be lowered
 function incOverallDepth() {
   overallDepth++;
-  console.log("depth= " + overallDepth);
   if(overallDepth > MAX_DEPTH) {
     overallDepth = Math.floor(overallDepth / 2) + 1;
     Object.keys(objectInfoToSend).forEach(key => {
